11d21381d20772f1a8c415cc7a6c1d49bca2e964
Lucas-X-A <lucasxavierdealmeida@gmail.com>
2025-06-15 14:52:36 -0300

    Add: Estrutura básica

---
+++ src/main/java/br/com/projetonotificador/App.java
@@ -0,0 +1,13 @@
+package br.com.projetonotificador;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/br/com/projetonotificador/GerenciadorCompromissos.java
@@ -0,0 +1,4 @@
+package br.com.projetonotificador;
+
+public class GerenciadorCompromissos {
+}
