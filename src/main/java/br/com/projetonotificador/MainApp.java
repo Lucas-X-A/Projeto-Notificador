c9e4444adb98ac14c04cf61d5306ed1c00e0e5c6
Lucas-X-A <lucasxavierdealmeida@gmail.com>
2025-07-15 16:08:06 -0300

    fix: remoção de mensagem no terminal

--- src/main/java/br/com/projetonotificador/MainApp.java
+++ src/main/java/br/com/projetonotificador/MainApp.java
@@ -27,7 +27,6 @@ public class MainApp extends Application {
     public static void main(String[] args) {
         if (args.length > 0 && args[0].equals("--verificar")) {
             // Modo silencioso: apenas verifica e notifica
-            System.out.println("Modo de verificação ativado...");
             Notificador notificador = new Notificador();
             boolean notificou = notificador.verificarEAlertar();
             
